de95a1c2a4062d2a106522088a4791970bbd4958
DineshSethu001 <dineshsethu15981@gmail.com>
2023-12-04 11:25:21 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import Skills from './pages/Skills';
-import './styles/Astyles.scss'
+import { BrowserRouter as Router} from "react-router-dom";
+import Header from "./components/Header/Header";
+import './index.css'
+// import Home from "./pages/Home";
+// import Projects from "./pages/Projects";
+// import Contact from "./pages/Contact";
+// import Skills from "./pages/Skills";
+
 
 function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/"          element={<Home/>}     />
-        <Route path="/projects"  element={<Projects/>} />
-        <Route path="/contact"   element={<Contact/>}  />
-        <Route path="/skills"    element={<Skills/>}/>
-      </Routes>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/skills" element={<Skills />} />
+      </Routes> */}
     </Router>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,63 @@
+import { Link } from "react-router-dom";
+import { DiCssdeck } from "react-icons/di";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import './HeaderStyles.css'
+
+
+export default function Header() {
+  return (
+    <div className="container">
+      <div className="Div1">
+        <Link to="/">
+          <a style={{ display: "flex", alignItems: "center", color: "white" }}>
+            <DiCssdeck size="3rem" /> <span>Portfolio</span>
+          </a>
+        </Link>
+      </div>
+      <div className="Div2">
+        <li>
+          <Link to="#projects">
+            <a className='NavLink'>Projects</a>
+          </Link>
+        </li>
+        <li>
+          <Link to="#tech">
+            <a className="NavLink">Technologies</a>
+          </Link>
+        </li>
+        <li>
+          <Link to="#about">
+            <a className="NavLink">About</a>
+          </Link>
+        </li>
+      </div>
+       <div className="Div3">
+         <a className="SocialIcons" href="https://google.com">
+           <AiFillGithub size="3rem" />
+         </a>
+         <a className="SocialIcons" href="https://google.com">
+           <AiFillLinkedin size="3rem" />
+         </a>
+         <a className="SocialIcons" href="https://google.com">
+           <AiFillInstagram size="3rem"/>
+         </a>
+       </div>
+    </div>
+  );
+}
+
+//
+//     </Div2>
+// const Header = () =>  (
+//   <Container>
+//     <Div1>
+//       <Link href="/">
+//         <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
+//           <DiCssdeck size="3rem" /> <span>Portfolio</span>
+//         </a>
+//       </Link>
+//     </Div1>
+//
+
+//     </Container>
+// );
