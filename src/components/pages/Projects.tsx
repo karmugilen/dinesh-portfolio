20f11ab6afa11011d60994b11a15c9adbeecbb51
unknown <karmugil251@gmail.com>
2024-01-09 17:42:19 -0800

    "all"

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,9 +1,13 @@
 
-
 export default function Projects() {
   return (
     <div>
-      <h1>Project</h1>
-    </div>
+  <h1>Project</h1>
+  <ul>
+    <li>Project 1: Description or details about Project 1</li>
+    <li>Project 2: Description or details about Project 2</li>
+  </ul>
+</div>
+
   )
 }
